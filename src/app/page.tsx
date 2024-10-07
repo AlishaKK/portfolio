091ea44b72801d44122cc2a2b0386a1e14af9d4c
AlishaKK <nidakayani043@gmail.com>
2024-10-07 14:33:16 +0500

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
-import Hero from '@/components/Hero'
-import Projects from '@/components/Projects'
-import React from 'react'
+import ContactMe from '@/components/Contact';
+import Hero from '@/components/Hero';
+import Projects from '@/components/Projects';
+import Skills from '@/components/Skills';
+import React from 'react';
 
 export default function Home() {
   return (
-    <main  className='p-5'><Hero/>
-    <Projects/></main>
-  )
+    <main className="bg-black min-h-screen py-10">
+      <section className="container mx-auto p-5">
+        <Hero />
+      </section>
+      
+      <section className="container mx-auto p-5 mt-10 bg-black text-white shadow-lg rounded-lg">
+        <Projects />
+      </section>
+
+      <section className="container mx-auto p-5 mt-10 bg-black text-white shadow-lg rounded-lg">
+        <Skills />
+      </section>
+
+      <section className="container mx-auto p-5 mt-10 bg-black text-white shadow-lg rounded-lg">
+        <ContactMe />
+      </section>
+    </main>
+  );
 }
